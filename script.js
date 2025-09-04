e6fae33228079c1f16b78c118ace824efbf81859
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-09-04 12:56:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
 function getFormvalue(event) {
-  event.preventDefault();
+  event.preventDefault(); 
 
   let firstName = document.forms["form1"]["fname"].value.trim();
   let lastName = document.forms["form1"]["lname"].value.trim();
 
-  if (!firstName && !lastName) {
-    alert("Please enter your first and last name.");
-    return;
-  } else if (!firstName) {
-    alert("Please enter your first name.");
-    return;
-  } else if (!lastName) {
-    alert("Please enter your last name.");
-    return;
-  }
-
-  let fullName = firstName + " " + lastName;
+  let fullName = `${firstName} ${lastName}`;
   alert(fullName);
 }
 
