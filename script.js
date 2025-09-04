9f3cbb124549a2cb2c679b764b4752c930f03411
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-09-04 12:52:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+  event.preventDefault();
 
+  let firstName = document.forms["form1"]["fname"].value.trim();
+  let lastName = document.forms["form1"]["lname"].value.trim();
+
+  if (!firstName && !lastName) {
+    alert("Please enter your first and last name.");
+    return;
+  } else if (!firstName) {
+    alert("Please enter your first name.");
+    return;
+  } else if (!lastName) {
+    alert("Please enter your last name.");
+    return;
+  }
+
+  let fullName = firstName + " " + lastName;
+  alert(fullName);
 }
+
+document.getElementById("form1").addEventListener("submit", getFormvalue);
